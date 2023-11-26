1fe567e314de767a008d8d044d6c773f52ffcc1b
unknown <jamie.gworld@hotmail.com>
2023-11-26 20:04:36 +0700

    OK

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -78,6 +78,90 @@ double PmergeMe::getTimeList() const
 	return (_timeList);
 }
 
+void PmergeMe::merge_insertion_Deque()
+{
+	bool			oddFlag = false;
+	int				separated;
+	int				bsLimit;
+	clock_t			start;
+	clock_t			end;
+
+	oddFlag = false;
+	std::deque<int> cpyvalDeque;
+	std::deque<std::deque<int>> pairs;
+	std::deque<int> sorted;
+	std::deque<int> tosort;
+	cpyvalDeque = _valDeque;
+	start = clock();
+	//cas ou deque est de taille 1 (deja triée)
+	if (_valDeque.size() <= 1)
+	{
+		sorted = _valDeque;
+		end = clock();
+		//convertion en secondes (CLOCKS_PER_SEC représente les coups d'horloge par seconde)
+		_timeDeque = double((end - start) / double(CLOCKS_PER_SEC));
+		return ;
+	}
+	//gestion des éléments impairs
+	if (cpyvalDeque.size() % 2 == 1)
+	{
+		oddFlag = true;
+		separated = cpyvalDeque.back();
+		cpyvalDeque.pop_back();
+	}
+	//parcourir la copie en sautant de deux en deux, 
+	//et chaque paire triée est stockée dans une deque pairs
+	//(pairs est une deque de deque de deux entiers)
+	for (size_t i = 0; i < cpyvalDeque.size(); i = i + 2)
+	{
+		std::deque<int> temp;
+		if (cpyvalDeque[i] > cpyvalDeque[i + 1])
+		{
+			temp.push_back(cpyvalDeque[i]);
+			temp.push_back(cpyvalDeque[i + 1]);
+		}
+		else
+		{
+			temp.push_back(cpyvalDeque[i + 1]);
+			temp.push_back(cpyvalDeque[i]);
+		}
+		pairs.push_back(temp);
+	}
+	//trier pairs avec le tri fusion
+	mergeDeque(pairs.begin(), pairs.end());
+	//parcourir chaque deque de pairs et ajouter les premiers elements dans sorted
+	//puis les seconds elements dans tosort qui sera utilisée ultérieurement pour 
+	//l'insertion dans sorted
+	for (size_t i = 0; i < pairs.size(); i++)
+	{
+		sorted.push_back(pairs[i].front());
+		tosort.push_back(pairs[i].back());
+	}
+	//First element in tosort is lower than first element of sorted
+	//so it safe to add push it to front of sorted.
+	sorted.push_front(tosort.front());
+	tosort.pop_front();
+	bsLimit = 2;
+	while (!tosort.empty())
+	{
+		std::deque<int>::iterator it = sorted.begin();
+		std::advance(it, bsLimit);
+		it = std::lower_bound(sorted.begin(), it, tosort.front());
+		sorted.insert(it, tosort.front());
+		tosort.pop_front();
+		bsLimit += 2;
+	}
+	if (oddFlag)
+	{
+		std::deque<int>::iterator it = std::lower_bound(sorted.begin(),
+				sorted.end(), separated);
+		sorted.insert(it, separated);
+	}
+	_sortedDeque = sorted;
+	end = clock();
+	_timeDeque = double((end - start) / double(CLOCKS_PER_SEC));
+}
+
 //comparer deux deques avec le premier element
 bool PmergeMe::cmpDeque(const std::deque<int> &a,
 		const std::deque<int> &b)
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -21,7 +21,7 @@ class PmergeMe
 		~PmergeMe();
 		PmergeMe &operator=(const PmergeMe &src);
 
-		//Getter
+		//Getters
 		const std::deque<int>		&getValDeque() const;
 		const std::list<int>		&getValList() const;
 		const std::deque<int>		&getSortedDeque() const;
@@ -30,8 +30,36 @@ class PmergeMe
 		double						getTimeList() const;
 
 		//Methods
+		void	merge_insertion_Deque();
+		void	merge_insertion_List();
 		static bool cmpDeque(const std::deque<int>& a, const std::deque<int>& b);
 		static bool cmpList(const std::list<int>& a, const std::list<int>& b);
+
+		//Function Templates
+		template <class Iterator>
+		void mergeDeque(Iterator begin, Iterator end) {
+			if (end <= begin + 1) return;
+			Iterator middle = begin + (end - begin) / 2;
+			mergeDeque(begin, middle);
+			mergeDeque(middle, end);
+			std::inplace_merge(begin, middle, end, cmpDeque);
+		}
+
+		template <class Iterator>
+		void mergeList(Iterator begin, Iterator end) {
+			if (std::distance(begin, end) <= 1) return;
+
+			Iterator middle = begin;
+			std::advance(middle, std::distance(begin, end) / 2);
+
+			mergeList(begin, middle);
+			mergeList(middle, end);
+
+			std::list<typename Iterator::value_type> temp;
+			std::merge(begin, middle, middle, end, std::back_inserter(temp), cmpList);
+
+			std::copy(temp.begin(), temp.end(), begin);
+		}
 	
 	private:
 		std::deque<int>		_valDeque;
